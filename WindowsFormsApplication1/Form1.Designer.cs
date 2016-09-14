b18d356686535ec768bf577aed515e9d8f0562da
masaya <cisystest@gmail.com>
2016-09-14 09:45:55 +0900

    rev1

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(168, 138);
+            this.checkBox1.Location = new System.Drawing.Point(59, 152);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 16);
             this.checkBox1.TabIndex = 1;
